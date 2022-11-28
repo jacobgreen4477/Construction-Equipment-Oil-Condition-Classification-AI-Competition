4a8586ff66f87b7ce5e3d7dedfb63f545e3e549b
현종열 <jacobgreen4477@gmail.com>
2022-11-28 09:29:04 +0900

    Create display_importances.py

---
+++ display_importances.py
@@ -0,0 +1,8 @@
+def display_importances(feature_importance_df_):
+    cols = feature_importance_df_[["feature", "importance"]].groupby("feature").mean().sort_values(by="importance", ascending=False)[:40].index
+    best_features = feature_importance_df_.loc[feature_importance_df_.feature.isin(cols)]
+    plt.figure(figsize=(8, 10))
+    sns.barplot(x="importance", y="feature", data=best_features.sort_values(by="importance", ascending=False))
+    plt.title('LightGBM Features (avg over folds)')
+    plt.tight_layout()
+    plt.savefig('lgbm_importances01.png')
