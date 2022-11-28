ceadbce0326eb425065795f876aad98ac95eaca8
현종열 <jacobgreen4477@gmail.com>
2022-11-28 10:00:00 +0900

    Update permutation_lgb_vi.py

--- permutation_lgb_vi.py
+++ permutation_lgb_vi.py
@@ -1,3 +1,6 @@
+from DMS_202211.common import common
+from DMS_202211.seed_everything import seed_everything
+
 def permutation_lgb_vi(train, params, drop_features):
     
     params = {'application':'binary', 'metric':'auc'}
