a98b900c9b2ef8e1a12094026f89698b303aca93
현종열 <jacobgreen4477@gmail.com>
2022-11-28 10:04:50 +0900

    Create num_feature.py

---
+++ num_feature.py
@@ -0,0 +1,16 @@
+from DMS_202211.common import common
+from DMS_202211.seed_everything import seed_everything
+
+def num_feature(df,num_features):
+    if num_features[0][:5] == 'rank_':
+        num_agg_df = df.groupby("YEAR",sort=False)[num_features].agg(['last'])
+    else:
+        num_agg_df = df.groupby("YEAR",sort=False)[num_features].agg(['mean', 'std', 'min', 'max', 'sum'])
+    num_agg_df.columns = ['_'.join(x) for x in num_agg_df.columns]
+    if num_features[0][:5] != 'rank_':
+        for col in num_agg_df.columns:
+            num_agg_df[col] = num_agg_df[col] // 0.01
+    df = num_agg_df.reset_index()
+    print('num feature shape after engineering', df.shape )
+
+    return df
