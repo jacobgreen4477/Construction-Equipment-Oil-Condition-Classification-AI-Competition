315c9dfd77a21b7b4ef59d38ea6f777c759ff9e3
현종열 <jacobgreen4477@gmail.com>
2022-11-28 10:03:50 +0900

    Create diff_feature.py

---
+++ diff_feature.py
@@ -0,0 +1,17 @@
+from DMS_202211.common import common
+from DMS_202211.seed_everything import seed_everything
+
+def diff_feature(df):
+    diff_num_features = [f'diff_{col}' for col in num_features]
+    cids = df['YEAR'].values
+    df = df.groupby('YEAR')[num_features].diff().add_prefix('diff_')
+    df.insert(0,'YEAR',cids)
+    num_agg_df = df.groupby("YEAR",sort=False)[diff_num_features].agg(['mean', 'std', 'min', 'max', 'sum'])
+    num_agg_df.columns = ['_'.join(x) for x in num_agg_df.columns]
+    for col in num_agg_df.columns:
+        num_agg_df[col] = num_agg_df[col] // 0.01
+
+    df = num_agg_df.reset_index()
+    print('diff feature shape after engineering', df.shape )
+
+    return df
