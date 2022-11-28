349d1d1ede240561f7a3beb9bc123c5bdf509182
현종열 <jacobgreen4477@gmail.com>
2022-11-28 10:19:19 +0900

    Update train_model.py

--- train_model.py
+++ train_model.py
@@ -1,4 +1,4 @@
-from DMS_202211.common import common
+from DMS_202211 import common
 from DMS_202211.seed_everything import seed_everything
 
 def train_model(train,test,params,stratified,num_folds,drop_features,seed_num=1):
